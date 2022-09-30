69af7b04b56434f393b09470ba003e80f18f3587
Antoine <antoine.pavy92@gmail.com>
2022-09-30 09:40:14 +0200

    Added ingredient effects

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -4,16 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable] public enum IngredientType{ Vegetable = 1, Fruit = 2, Cereals = 3, Meat = 4, Fish = 5, Spice = 6, Herb = 7}
+[Serializable] public enum IngredientEffectType{ Sweet, Salty, Sour, Bitter, Umami, Cold, Hot}
 
 [Serializable]
 public class Ingredient : ScriptableObject
 {
     [SerializeField]
     private IngredientType type;
+    
+    [SerializeField]
+    private List<IngredientEffect> effects;
+    
     [SerializeField]
     private Sprite ingredientSprite;
+    
     [SerializeField]
     private int id;
+    
     public int GetHash()
     {
         return type.GetHashCode() * 100 + id;
---
+++ Assets/Scripts/IngredientEffect.cs
@@ -0,0 +1,7 @@
+﻿using System;
+[Serializable]
+public struct IngredientEffect
+{
+    public IngredientEffectType type;
+    public int intensity;
+}
